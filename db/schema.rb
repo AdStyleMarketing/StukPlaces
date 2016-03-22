6af50077d4c4581d9d428bc037e647d9a4b16f41
AdStyle Marketing <doug@adstylemarketing.com>
2016-03-22 11:46:58 -0500

    Added the average rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322154842) do
+ActiveRecord::Schema.define(version: 20160322163350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,10 +40,11 @@ ActiveRecord::Schema.define(version: 20160322154842) do
     t.string   "phone"
     t.string   "website"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "total_average", default: 0
   end
 
   create_table "rates", force: :cascade do |t|
