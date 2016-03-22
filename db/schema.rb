4e50dfd44a33b47cc3c71fcf2c7a3832bc1f9529
AdStyle Marketing <doug@adstylemarketing.com>
2016-03-22 11:29:58 -0500

    Added Raty for Rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322144522) do
+ActiveRecord::Schema.define(version: 20160322154842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,9 @@ ActiveRecord::Schema.define(version: 20160322144522) do
     t.integer  "user_id"
     t.integer  "place_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "score",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
